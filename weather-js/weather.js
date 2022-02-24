16d1292c7aeb6206944d300c8ae4ed47b3f2924f
eduardo andré <andremoraes930@gmail.com>
2022-02-24 10:21:14 -0300

    :up:

---
+++ weather-js/weather.js
@@ -0,0 +1,351 @@
+$(function() {
+
+  // *** APIs ***
+// clima, previsão 12 horas e previsão 5 dias: https://developer.accuweather.com/apis
+// pegar coordenadas geográficas pelo nome da cidade: https://docs.mapbox.com/api/
+// pegar coordenadas do IP: http://www.geoplugin.net
+// gerar gráficos em JS: https://www.highcharts.com/demo
+
+  var apikeyt =	"SzqMTen6C54RHwAfGyrUV2zTzCgHkhSA"; 
+
+  var mapboxtoken = "pk.eyJ1IjoiZWR1YXJkb2FuZHJlIiwiYSI6ImNrazh2MzBqczByaGEycXE4aTZkNndicGIifQ.t3x9Ke-nVNGC6duOdEACEQ";
+
+  var weatherObject = {
+    cidade: "",
+    estado: "",
+    pais: "",
+    temperatura: "",
+    texto_clima: "",
+    icone_clima: "", 
+  };
+
+  //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||//
+
+  // Define as coordenadas de IP 
+  function pegarCordenadasIP(){
+
+        var lat_padrao = -22.80483179823112;
+        var long_padrao = -47.24897972630037;
+    
+    $.ajax({
+        url : "http://www.geoplugin.net/json.gp",
+        type: "GET",
+        dataType: "json",
+    
+      success: function(data){
+
+      if(data.geoplugin_latitude && data.geoplugin_longitude){
+
+          pegarLocalUsuario(data.geoplugin_latitude, data.geoplugin_longitude);
+
+      }else{
+
+          pegarLocalUsuario(lat_padrao, long_padrao);
+      }
+      },
+      error: function(){
+
+          gerarErros("Erro na requisição"); 
+          pegarLocalUsuario(lat_padrao, long_padrao);
+      } 
+    });
+  }
+    pegarCordenadasIP();
+
+  //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||//
+
+  // Define as coordenadas de pesquisa digitada pelo usuario 
+  function  pegarCordPesq(input){
+
+      input = encodeURI(input);
+
+    $.ajax({
+        url : "https://api.mapbox.com/geocoding/v5/mapbox.places/" + input + ".json?access_token=" + mapboxtoken,
+        type: "GET",
+        dataType: "json",
+
+      success: function(data){
+
+          //console.log("mapbox: ", data);
+
+      try{
+
+          var long = data.features[0].geometry.coordinates[0];
+          var lat = data.features[0].geometry.coordinates[1];
+          pegarLocalUsuario(lat, long);
+
+      }catch{
+
+        gerarErros('Erro na pesquisa de local');
+      }
+      },  
+      error: function(){
+        gerarErros("Erro no mapbox");
+      }
+   });
+  }
+
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||//
+
+  // Define o local atual do usuario através das coordenadas de IP 
+  function pegarLocalUsuario (lat, long) {
+    
+    $.ajax({
+        url : "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=" + apikeyt + "&q=" + lat +"%2C" + long + "&language=pt-br",
+        type: "GET",
+        dataType: "json",
+    
+      success: function(data){
+        //console.log(data);
+
+      try {
+                  
+        weatherObject.cidade = data.ParentCity.LocalizedName;
+      }
+      catch{
+
+        weatherObject.cidade = data.LocalizedName;
+      }
+    
+        weatherObject.estado = data.AdministrativeArea.LocalizedName;
+        weatherObject.pais = data.Country.LocalizedName;
+
+      var localCode = data.Key;
+
+        pegarTempoAtual(localCode);
+        pegarTempo5Dias(localCode);
+        PegarPrevisaoHoraHora(localCode)
+      },
+      error: function(){
+        gerarErros('Erro no código do local');
+
+      }  
+    });
+  }
+
+  //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||//
+
+  // Responsável por gerar uma requisição do clima atual
+  function pegarTempoAtual (localCode) {
+
+    $.ajax({
+        url : "http://dataservice.accuweather.com/currentconditions/v1/" + localCode + "?apikey=" + apikeyt + "&language=pt-br",
+        type: "GET",
+        dataType: "json",
+      
+      success: function(data){
+        //console.log(data);
+
+        weatherObject.temperatura = data[0].Temperature.Metric.Value;
+        weatherObject.texto_clima = data[0].WeatherText;
+
+        var iconNumber = data[0].WeatherIcon <= 9 ? "0" + String(data[0].WeatherIcon) : String(data[0].WeatherIcon); 
+
+        weatherObject.icone_clima = "https://developer.accuweather.com/sites/default/files/" + iconNumber + "-s.png";
+
+        preencherClimaAgora(weatherObject.cidade,weatherObject.estado,weatherObject.pais,weatherObject.temperatura,weatherObject.texto_clima,weatherObject.icone_clima);
+      },
+      error: function(){
+        gerarErros('Erro ao obter clima atual');
+      }    
+    });
+  }
+
+  //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||//
+
+  // Responsável por preencher o gráfico com o clima atual 
+  function preencherClimaAgora(cidade,estado,pais,temperatura,texto_clima,icone_clima){
+
+      var texto_local = cidade + ", " + estado + ". " + pais;
+      $("#texto_local").text(texto_local);
+      $("#texto_clima").text(texto_clima);
+      $("#texto_temperatura").html(String(temperatura) + "&deg;");
+      $("#icone_clima").css("background-image", "url('" + weatherObject.icone_clima + "')");
+  }
+
+  //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||//
+
+  // Código do gráfico requisitada do site highcharts
+  function PegarGrafico(horas, temperaturas) {
+
+    Highcharts.chart('hourly_chart', {
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: 'Previsão Hora a Hora',
+            style: {color: 'blue'}
+        },
+        xAxis: {
+            categories: horas
+        },
+        yAxis: {
+            title: {
+                text: 'Temperaturas (°C)'
+            }
+        },
+        plotOptions: {
+            line: {
+                dataLabels: {
+                    enabled: false
+                },
+                enableMouseTracking: false
+            }
+        },
+        series: [{
+            showInLegend: false,            
+            data: temperaturas 
+        }]
+    });
+  }
+
+  //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||//
+
+  // Responsável por atualizar o gráfico de hora em hora
+  function PegarPrevisaoHoraHora(localCode){
+
+    $.ajax({
+        url : "http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/" + localCode + "?apikey=" + apikeyt + "&language=pt-br&metric=true",
+        type: "GET",
+        dataType: "json",
+
+      success: function(data){
+
+        var horarios = [];
+        var temperaturas = [];
+
+      for(var a = 0; a < data.length; a++){
+
+        var hora= new Date(data[a].DateTime).getHours();  
+        horarios.push(String(hora) + "h");
+
+        temperaturas.push( data[a].Temperature.Value )
+
+        PegarGrafico(horarios, temperaturas);
+        $('.refresh-loader').fadeOut();
+      }
+      },
+      error: function(){
+
+        gerarErros('Erro ao obter previsão hora a hora');
+      }  
+    });
+  };
+
+  //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||//
+
+  // Responsável por gerar uma requisição do clima por até 5 dias
+  function pegarTempo5Dias(localCode){  
+
+    $.ajax({
+        url : "http://dataservice.accuweather.com/forecasts/v1/daily/5day/" + localCode + "?apikey=" + apikeyt + "&language=pt-br&metric=true",
+        type: "GET",
+        dataType: "json",
+
+      success: function(data){
+
+        //console.log("5 dias: ", data);
+      
+        $('#texto_max_min').html(String(data.DailyForecasts[0].Temperature.Minimum.Value) + "&deg: /" + String(data.DailyForecasts[0].Temperature.Maximum.Value) + "&deg:");
+                    
+        preencherTempo5Dias(data.DailyForecasts);
+      },
+      error: function(){
+
+        gerarErros('Erro ao obter previsão de 5 dias');
+      }  
+    });
+  }
+
+  //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||//
+
+  // Responsável por preencher o gráfico de 5 dias
+  function preencherTempo5Dias(previsoes){
+
+      $('#info_5dias').html("");
+
+        var diasdaSemana = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+
+      for(var a = 0; a < previsoes.length; a++){
+
+        var datehoje = new Date(previsoes[a].Date);
+        var dia_semana = diasdaSemana[datehoje.getDay()];
+
+        var iconNumber = previsoes[a].Day.Icon <= 9 ? "0" + String(previsoes[a].Day.Icon) : String(previsoes[a].Day.Icon); 
+
+        iconeClima = "https://developer.accuweather.com/sites/default/files/" + iconNumber + "-s.png";
+        minima = String(previsoes[a].Temperature.Minimum.Value);
+        maxima = String(previsoes[a].Temperature.Maximum.Value);
+
+        elementoHtmlDia =  '<div class="day col">';
+        elementoHtmlDia += '<div class="day_inner">';
+        elementoHtmlDia += '<div class="dayname">';
+        elementoHtmlDia += dia_semana;
+        elementoHtmlDia += '</div>';
+        elementoHtmlDia += '<div style="background-image: url(\'' + iconeClima + '\')" class="daily_weather_icon"></div>';
+        elementoHtmlDia += '<div class="max_min_temp">';
+        elementoHtmlDia +=  minima + '&deg; / ' + maxima + '&deg;';
+        elementoHtmlDia += '</div>';
+        elementoHtmlDia +=  '</div>';
+        elementoHtmlDia += '</div>'; 
+        
+        $('#info_5dias').append(elementoHtmlDia);
+        elementoHtmlDia = "";
+    }
+  }
+ 
+ //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||//
+ 
+ // Responsável para informar sobre a manifestação de erros
+  function gerarErros(mensagem){
+      if(!mensagem){
+          mensagem = "Erro na solicitação";
+      }
+          $('.refresh-loader').hide();
+          $('#aviso-erro').text(mensagem);
+          $('#aviso-erro').slideDown();
+
+      window.setTimeout(function(){
+          $('#aviso-erro').slideUp();
+    },4000);
+  }
+  
+  
+  // Pesquisar com a tecla enter 
+
+  $('input#local').on('keypress', function(e){
+      if(e.which == 13){
+          $('.refresh-loader').show();
+
+          var local = $('input#local').val();
+      if(local){
+          pegarCordPesq(local); 
+          document.querySelector('#local').value = ""; 
+      }else{
+            alert('Local Invalido');
+      }
+    }  
+    
+  });
+
+  // Pesquisar com a tecla normal 
+
+  $('#search-button').click(function(){
+
+  $('.refresh-loader').show();
+          var local = $('input#local').val();
+         
+      if(local){
+          pegarCordPesq(local);
+         
+      }else{
+          alert('Local Invalido');
+      }
+      document.querySelector('#local').value = ""; 
+  });
+
+
+
+  
+  
+  });
